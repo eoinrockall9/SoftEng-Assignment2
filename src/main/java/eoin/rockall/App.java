3d60f4873d2a7dc4652f9dc21b256e7c7cb49534
Eoin Rockall <e.rockall9@gmail.com>
2019-11-28 13:24:20 +0000

    inital maven project created

---
+++ src/main/java/eoin/rockall/App.java
@@ -0,0 +1,13 @@
+package eoin.rockall;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
